afe4ba559885214b3ad9d42ddf7f64d427a93e83
joaoVGvieira <joaovitorgv2019@gmail.com>
2022-12-10 14:38:14 -0300

    att dados

--- Sources/ler.c
+++ Sources/ler.c
@@ -4,26 +4,26 @@ void leitura( char* caminhoArquivo ){
     
     FILE *arq, *arqE;
     char *result;
-    char Linha[100];
+    char Linha[1000];
     int i=0;
     arq = fopen( caminhoArquivo, "r" );
-    arqE = fopen("teste.txt","w");
+    //arqE = fopen("teste.txt","w");
     if ( arq == NULL ) {
         printf("\nARQUIVO NAO ENCONTRADO\n");
+
         return;
     }
     else {
         printf("\nLEITURA DE ARQUIVO FEITA COM SUCESSO!\n");
         while (!feof(arq))
         {
-            result = fgets(Linha, 100, arq);  // o 'fgets' lê até 99 caracteres ou até o '\n'
-            // acima a gente tem que ver qual valor que vamo usar para verificar a sequencia de caracterer lá
-            // acho que é necesario usar essa variavel para armazenar essa a sequencia do tamanho que vamo definir ainda
-            // fiz um teste criando um arquivo so pra ver se tava lendo tudo ok
-            fputs(Linha,arqE);
+           fscanf(arq, "%s", Linha);
+           //aqui chama alguma funçao
+           //fprintf(arqE,"%s", Linha);
+            
         }
         fclose( arq );
-        fclose(arqE);
+        //fclose(arqE);
     }
     
 }
