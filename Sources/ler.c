99185c51bcd8179e6db0ea289b928b12bca6db01
Leonardo-Araujo-Resende <leonardo.a.aguiar@ufv.br>
2022-12-10 16:03:13 -0300

    ler

--- Sources/ler.c
+++ Sources/ler.c
@@ -1,24 +1,40 @@
 #include "../Libs/ler.h"
+#include <string.h>
 
 void leitura( char* caminhoArquivo ){
     
     FILE *arq, *arqE;
     char *result;
-    char Linha[1000];
+    char Linha[100000];
+    int i=0;
+    char registro[1000];
+    int fim = 0, cont = -1;
     arq = fopen( caminhoArquivo, "r" );
-    arqE = fopen("teste.txt","w");
+    //arqE = fopen("teste.txt","w");
     if ( arq == NULL ) {
         printf("\nARQUIVO NAO ENCONTRADO\n");
+
         return;
     }
     else {
         printf("\nLEITURA DE ARQUIVO FEITA COM SUCESSO!\n");
-        while (!feof(arq))
-        {
-            result = fgets(Linha, 1000, arq);  
-            fputs(Linha,arqE);
+
+        while (!feof(arq)){
+
+            fscanf(arq, "%s", Linha);
+            cont ++;
+            for(int i = 0; i < 1000; i ++){
+                if(Linha[i]== '\0') break;
+                printf("%c", Linha[i]);
+            }
+            printf(" cont = %d\n", cont);
         }
+
+
+        printf("Quantidade de registros %d", cont);
+
         fclose( arq );
-        fclose(arqE);
+        //fclose(arqE);
     }
+    
 }
