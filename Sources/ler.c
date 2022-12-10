b463a059cbb4cd0451d0d1e63193a1735614fe68
joaoVGvieira <joaovitorgv2019@gmail.com>
2022-12-10 15:47:41 -0300

    att arquivo lido

--- Sources/ler.c
+++ Sources/ler.c
@@ -5,25 +5,20 @@ void leitura( char* caminhoArquivo ){
     FILE *arq, *arqE;
     char *result;
     char Linha[1000];
-    int i=0;
     arq = fopen( caminhoArquivo, "r" );
-    //arqE = fopen("teste.txt","w");
+    arqE = fopen("teste.txt","w");
     if ( arq == NULL ) {
         printf("\nARQUIVO NAO ENCONTRADO\n");
-
         return;
     }
     else {
         printf("\nLEITURA DE ARQUIVO FEITA COM SUCESSO!\n");
         while (!feof(arq))
         {
-           fscanf(arq, "%s", Linha);
-           //aqui chama alguma funçao
-           //fprintf(arqE,"%s", Linha);
-            
+            result = fgets(Linha, 1000, arq);  
+            fputs(Linha,arqE);
         }
         fclose( arq );
-        //fclose(arqE);
+        fclose(arqE);
     }
-    
 }
