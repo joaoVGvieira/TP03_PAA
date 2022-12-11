908f7680cd30f60014a636b98a71e8092c6a54ce
Dusstody <gabriel.custodio@ufv.br>
2022-12-11 12:57:23 -0300

    Adição: Interface Similaridade

--- main.c
+++ main.c
@@ -17,11 +17,29 @@ int main()
         scanf("%d",&opc);
         switch (opc)
         {
+        case 0 :
+            break;
         case 1 :
             func(); 
             break;
-        case 0:
+        case 2 :
+            printf(
+                "\n ______________Similaridade Entre Pares______________ \n");
+            
+            clock_t inicioBoyerMoore = clock();
+            
+            printf("|                                                    |\n");
+            printf("|_________________Humano e Chimpanzé_________________|\n");
+            //printf("|                      %.15lf                        |\n", similaridade);
+            printf("|_________________Humano e Cachorro__________________|\n");
+            //printf("|                      %.15lf                        |\n", similaridade);
+            printf("|________________Chimpanzé e Cachorro________________|\n");
+            //printf("|                      %.15lf                        |\n", similaridade);
+            printf("|                                                    |\n");
+            printf("|____________________________________________________|\n");
+
             break;
+
         default:
             break;
         }
